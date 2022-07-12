3a617b10d0779e4cd63b307e68e350bff6d87ea4
Calum Shepherd <calum.shep@icloud.com>
2022-07-12 16:46:27 +0100

    Install Breeze auth and port to Bootstrap from Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/css/app.css',
             'resources/js/app.js',
-        ]),
+        ], {
+            refresh: true,
+        }),
     ],
+    server: {
+        host: '192.168.56.56',
+        watch: {
+            usePolling: true,
+        },
+    },
 });
